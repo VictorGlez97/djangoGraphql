33ae6225426452d4eb636fb355160bf187e63403
vhgonzalez <vh.gonzalez@mavi.mx>
2025-04-01 18:53:05 -0600

    test para obtener str binario y convertir a pdf

--- strawberry_django_tut/crud/schema.py
+++ strawberry_django_tut/crud/schema.py
@@ -11,6 +11,11 @@ from dataclasses import asdict
 
 from asgiref.sync import sync_to_async
 
+import requests
+import json
+from dataclasses import asdict
+import base64
+
 @strawberry.input
 class BookInput:
     title: str
@@ -38,6 +43,33 @@ class Query:
     def all_books(self) -> List[BookType]:
         return Book.objects.all()
 
+@strawberry.type
+class ResponseType:
+    success: bool
+    data: Optional[str] = None
+    error: Optional[str] = None
+
+@strawberry.input
+class ParamsData:
+    Nombre: str
+    Paterno: str
+    Materno: str
+    Fecha: str
+
+@strawberry.input
+class DataFetch:
+    FormatoArchivo: str
+    Archivo: str
+    parameters: Optional[ParamsData]
+    reportDataSet: List[str]
+
+@strawberry.type
+class ResponseReport:
+    Servicio: Optional[str]
+    Mensaje: Optional[str]
+    Message: Optional[str]
+    Estatus: Optional[int]
+    Data: str
 
 @strawberry.type
 class Mutation:
@@ -69,5 +101,51 @@ class Mutation:
             return True
         except Book.DoesNotExist:
             raise strawberry('Not found')
+        
+    @strawberry.mutation
+    async def fetch_endpoint(self, data: Optional[DataFetch]) -> str:
+        try:
+
+            urlToken = 'http://admin.bposspro.com:7849/identity/connect/token'
+
+            headersToken = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+            payloadToken = {
+                'client_id': '8e8c6bad8061deec8841fa8f910a1203',
+                'scope': 'bps_api',
+                'client_secret': 'dd58b6ca211a429250f5eed7701d4003',
+                'grant_type': 'client_credentials'
+            }
+
+            responseToken = await sync_to_async(requests.post)(urlToken, data=payloadToken, headers=headersToken, timeout=60)
+            responseToken.raise_for_status()
+
+            ResponseType(success=True, data=responseToken.text)
+            
+            access_token = responseToken.json().get('access_token')
+
+            if access_token:
+                
+                url = 'http://admin.bposspro.com:7849/api/v1.0/reporte/GenerarReporte'
+
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': f'Bearer {access_token}'
+                }
+
+                jsonData = json.dumps(asdict(data))
+
+                response = await sync_to_async(requests.post)(url, data=jsonData, headers=headers, timeout=60)
+                response.raise_for_status()
+                
+                pdfBytes = base64.b64decode(response.json()['Data']).decode('utf-8')
+                print(pdfBytes)
+
+                with open('archivo_descarga.pdf', 'wb') as f:
+                    f.write(base64.b64decode(pdfBytes))
+
+                return ResponseType(success=True, data=pdfBytes)
+        except requests.RequestException as e:
+            return ResponseType(success=False, error=str(e))
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
--- strawberry_django_tut/strawberry_django_tut/settings.py
+++ strawberry_django_tut/strawberry_django_tut/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 'corsheaders'
     # 'book',
     'crud',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ALLOW ALL REQUESTS
+CORS_ALLOW_ALL_ORIGINS = True
+
+# ALLOW A LIST OF DOMINES
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:5173',
+#     'http://127.0.0.1:5173',
+# ]
+
+CORS_ALLOW_METHODS = [
+    'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type', 'authorization', 'X-Requested-With'
+]
+
